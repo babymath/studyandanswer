b30bc92e49d5f5311391cfaf11665f078a47ed17
babymath <164469039+babymath@users.noreply.github.com>
2025-02-22 14:03:18 +0530

    Add files via upload

---
+++ books.js
@@ -0,0 +1,12 @@
+const books = [
+  { 
+    class: "Class 11", 
+    publication: "NCERT", 
+    authors: [], 
+    subject: "Mathematics", 
+    image: "aaa/img/math-ncert-class11.jpg", 
+    link: "books/math-ncert-class11", 
+    colour: "rgb(181, 193, 247)",
+    title: "class 11, ncert, mathematics" 
+  }
+];
